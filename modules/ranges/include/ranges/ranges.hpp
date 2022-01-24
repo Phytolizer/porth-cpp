f304beec345484f7fc4786b708a8397f6e75e9ad
Kyle Coffey <mariorules24@gmail.com>
2022-01-24 13:25:36 -0600

    Approach the problem differently.

--- modules/ranges/include/ranges/ranges.hpp
+++ modules/ranges/include/ranges/ranges.hpp
@@ -5,23 +5,12 @@
 #include <ranges>
 
 namespace my_ranges {
-template <std::ranges::input_range Range, std::weakly_incrementable Out>
-requires std::indirectly_copyable<std::ranges::iterator_t<Range>, Out>
-constexpr std::ranges::copy_result<std::ranges::borrowed_iterator_t<Range>, Out> copy(Range&& range, Out out) {
-    return std::ranges::copy(std::move(range), out);
-}
+using namespace std::ranges;
 } // namespace my_ranges
 #else
 #include <range/v3/algorithm/copy.hpp>
 
 namespace my_ranges {
-/// \overload
-template <typename Rng, typename O>
-/// \pre
-requires ranges::input_range<Rng> && ranges::weakly_incrementable<O> &&
-    ranges::indirectly_copyable<ranges::iterator_t<Rng>, O>
-constexpr ranges::copy_result<ranges::borrowed_iterator_t<Rng>, O> copy(Rng&& rng, O out) {
-    return ranges::copy(rng, out);
-}
+using namespace ranges;
 } // namespace my_ranges
 #endif
