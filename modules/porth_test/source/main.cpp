90cae1640835ca9d60fa8f1e84aee1260eced0ac
Kyle Coffey <mariorules24@gmail.com>
2022-01-24 22:00:45 -0600

    Rewrite the tester to use expected output.

--- modules/porth_test/source/main.cpp
+++ modules/porth_test/source/main.cpp
@@ -1,8 +1,10 @@
 #include <algorithm>
 #include <cstddef>
 #include <filesystem>
+#include <fstream>
 #include <iostream>
 #include <regex>
+#include <span/span.hpp>
 #include <sstream>
 #include <stdexcept>
 #include <string>
@@ -64,7 +66,7 @@ std::string runSubprocess(const std::vector<std::string>& args) {
     return result;
 }
 
-int main() {
+int test() {
     const std::filesystem::recursive_directory_iterator tests{std::filesystem::current_path() / "tests"};
     for (const auto& entry : tests) {
         if (entry.is_directory()) {
@@ -75,10 +77,41 @@ int main() {
         }
 
         std::cout << "[INFO] Testing " << entry.path().filename().string() << "\n";
+
+        std::filesystem::path txtPath = entry.path();
+        txtPath.replace_extension(".txt");
+        std::ifstream txtFile{txtPath.string()};
+        std::ostringstream txtContentsStream;
+        if (!(txtContentsStream << txtFile.rdbuf())) {
+            std::cerr << "[ERROR] failed to read " << txtPath.string() << "\n";
+            return 1;
+        }
+        const std::string expectedOutput = txtContentsStream.str();
+
         try {
             const std::string simOutput = runSubprocess({PORTH_CPP_EXE, "sim", entry.path().string()});
+            if (simOutput != expectedOutput) {
+                std::cerr << "[ERROR] Unexpected simulation output\n";
+                std::cerr << "  Expected:\n";
+                std::istringstream expectedStream{expectedOutput};
+                std::string line;
+                while (std::getline(expectedStream, line)) {
+                    std::cerr << "    " << line << "\n";
+                }
+                std::cerr << "  Simulation output:\n";
+                std::istringstream simStream{simOutput};
+                while (std::getline(simStream, line)) {
+                    std::cerr << "    " << line << "\n";
+                }
+                return 1;
+            }
+
             std::filesystem::path exePath = entry.path();
+#ifdef _WIN32
+            exePath.replace_extension(".exe");
+#else
             exePath.replace_extension();
+#endif
             runSubprocess({
                 PORTH_CPP_EXE,
                 "com",
@@ -87,26 +120,90 @@ int main() {
                 entry.path().string(),
             });
             const std::string comOutput = runSubprocess({exePath});
-            if (simOutput != comOutput) {
-                std::cout << "[ERROR] Output discrepancy between simulation and compilation\n";
-                std::cout << "  Simulation output:\n";
-                std::istringstream simStream{simOutput};
+            if (comOutput != expectedOutput) {
+                std::cerr << "[ERROR] Unexpected compilation output\n";
+                std::cerr << "  Expected:\n";
+                std::istringstream expectedStream{expectedOutput};
                 std::string line;
-                while (std::getline(simStream, line)) {
-                    std::cout << "    " << line << "\n";
+                while (std::getline(expectedStream, line)) {
+                    std::cerr << "    " << line << "\n";
                 }
-                std::cout << "  Compilation output:\n";
+                std::cerr << "  Compilation output:\n";
                 std::istringstream comStream{comOutput};
                 while (std::getline(comStream, line)) {
-                    std::cout << "    " << line << "\n";
+                    std::cerr << "    " << line << "\n";
                 }
                 return 1;
-            } else {
-                std::cout << "[INFO] " << entry.path().filename().string() << " OK\n";
             }
+
+            std::cout << "[INFO] " << entry.path().filename().string() << " OK\n";
         } catch (const SubprocessError& e) {
             std::cout << "[ERROR] " << e.what() << "\n";
             return 1;
         }
     }
+    return 0;
+}
+
+void record() {
+    const std::filesystem::recursive_directory_iterator tests{std::filesystem::current_path() / "tests"};
+    for (const auto& entry : tests) {
+        if (entry.is_directory()) {
+            continue;
+        }
+        if (entry.path().extension() != ".porth") {
+            continue;
+        }
+
+        std::cout << "[INFO] Recording " << entry.path().filename().string() << "\n";
+
+        std::filesystem::path txtPath = entry.path();
+        txtPath.replace_extension(".txt");
+        std::ostringstream txtContentsStream;
+        try {
+            const std::string simOutput = runSubprocess({PORTH_CPP_EXE, "sim", entry.path().string()});
+            txtContentsStream << simOutput;
+        } catch (const SubprocessError& e) {
+            std::cout << "[ERROR] " << e.what() << "\n";
+            return;
+        }
+        std::ofstream txtFile{txtPath.string()};
+        if (!(txtFile << txtContentsStream.str())) {
+            std::cerr << "[ERROR] failed to write " << txtPath.string() << "\n";
+            return;
+        }
+    }
+}
+
+void usage(const std::string& exeName) {
+    std::cout << "Usage: " << exeName << " [SUBCOMMAND]\n";
+    std::cout << "  test      Run the tests. (Default when no subcommand is provided)\n";
+    std::cout << "  record    Record expected output for the tests.\n";
+    std::cout << "  help      Print this message to stdout.\n";
+}
+
+int main(int argc, char** argv) {
+    span::Span<char*> args{argv, static_cast<std::size_t>(argc)};
+    std::size_t cursor = 0;
+    const std::string exeName = args[cursor++];
+
+    if (args.size() == cursor) {
+        return test();
+    }
+
+    const std::string subcmd = args[cursor++];
+    if (subcmd == "record") {
+        record();
+        return 0;
+    }
+    if (subcmd == "test") {
+        return test();
+    }
+    if (subcmd == "help") {
+        usage(exeName);
+        return 0;
+    }
+    usage(exeName);
+    std::cerr << "[ERROR] Unknown subcommand '" << subcmd << "'\n";
+    return 1;
 }
